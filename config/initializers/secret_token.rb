036a0fa6f26275668bf563194f215b735ce097d6
Alan Elliott <alan@ils-bc.com>
2010-09-21 09:20:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Med::Application.config.secret_token = '00fa28480c70904cf468c1f4ba2b30bfc75436d127bd12079d448eec748b35bc875a8605ffa0e75db84e505608b4a91fd5df6c443454ab58242e823d46fddfb6'
